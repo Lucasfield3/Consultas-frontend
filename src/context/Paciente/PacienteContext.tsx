8fcc6c755cd3f7da6df7d2d87b0a4538fbed71de
Lucasfield3 <lucasclic@yahho.com.br>
2022-05-26 22:09:47 -0300

    alert paciente

--- src/context/Paciente/PacienteContext.tsx
+++ src/context/Paciente/PacienteContext.tsx
@@ -4,47 +4,47 @@ import { createPaciente, Paciente } from '../../services/Pacientes'
 import api from '../../services/utils/api';
 
 type PacienteContextData = {
-    registerPaciente: (data: Paciente) => Promise<Paciente | any> ;
-    pacientes:Paciente[];
-    getAllPacientes:()=> Promise<Paciente[] | any>;
+    registerPaciente: (data: Paciente) => Promise<Paciente | any>;
+    pacientes: Paciente[];
+    getAllPacientes: () => Promise<Paciente[] | any>;
 }
 
 type PacienteContextProviderProps = {
-     children: ReactNode
+    children: ReactNode
 }
 
 export const PacienteContext = createContext({} as PacienteContextData)
 
-export const PacienteContextProvider = ({children}: PacienteContextProviderProps) =>{
-    const [ pacientes, setPacientes ] = useState<Paciente[]>([])
+export const PacienteContextProvider = ({ children }: PacienteContextProviderProps) => {
+    const [pacientes, setPacientes] = useState<Paciente[]>([])
 
-    async function registerPaciente(data: Paciente):Promise<Paciente | any> {
+    async function registerPaciente(data: Paciente): Promise<Paciente | any> {
         const response = await createPaciente(data)
         const paciente = response.data as Paciente
 
-        if(paciente){
+        if (paciente) {
             console.log(paciente);
-            
+            alert('Paciente registrado.')
         }
     }
 
-    async function getAllPacientes():Promise<Paciente[] | any>{
+    async function getAllPacientes(): Promise<Paciente[] | any> {
         try {
-            if(getToken()){
-                const {data} = await api.get<Paciente[]>('/pacientes', 
-                { headers: {"Authorization" : `Bearer ${getToken()}`}})
+            if (getToken()) {
+                const { data } = await api.get<Paciente[]>('/pacientes',
+                    { headers: { "Authorization": `Bearer ${getToken()}` } })
                 setPacientes(data)
             }
         } catch (error) {
             console.log(error);
-            
-        }
-    }   
 
-     return(
-          <PacienteContext.Provider value={{getAllPacientes ,registerPaciente, pacientes}}>
-               {children}
-          </PacienteContext.Provider>
-     )
+        }
+    }
+
+    return (
+        <PacienteContext.Provider value={{ getAllPacientes, registerPaciente, pacientes }}>
+            {children}
+        </PacienteContext.Provider>
+    )
 
 }
